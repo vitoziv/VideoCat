cecc7fefc674c453e784bfbf83a21c4303059beb
Vito <vvitozhang@gmail.com>
2017-11-07 21:49:06 +0800

    Add VideoView

---
+++ VideoCat/Main/Util/UIStyle.swift
@@ -0,0 +1,21 @@
+//
+//  UIStyle.swift
+//  VideoCat
+//
+//  Created by Vito on 07/11/2017.
+//  Copyright © 2017 Vito. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static let titleColor = UIColor(white: 0.16, alpha: 1)
+    static let contentColor = UIColor(white: 0.26, alpha: 1)
+    static let detailColor = UIColor(white: 0.38, alpha: 1)
+}
+
+extension UIFont {
+    static let titleFont = UIFont(name: "PingFangSC-Semibold", size: 16) ?? UIFont.systemFont(ofSize: 16, weight: .semibold)
+    static let contentFont = UIFont(name: "PingFangSC-Light", size: 16) ?? UIFont.systemFont(ofSize: 16, weight: .light)
+    static let detailFont = UIFont(name: "PingFangSC-Light", size: 13) ?? UIFont.systemFont(ofSize: 13, weight: .light)
+}
---
+++ VideoCat/Main/ViewControllers/View/VideoView.swift
@@ -0,0 +1,140 @@
+//
+//  VideoView.swift
+//  VideoCat
+//
+//  Created by Vito on 06/11/2017.
+//  Copyright © 2017 Vito. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class VideoView: UIView {
+    
+    var player = VIPlayer()
+    
+    var controlView: VideoControlView!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        player.delegate = self
+        
+        let playerView = player.playerView
+        addSubview(playerView)
+        
+        controlView = VideoControlView()
+        addSubview(controlView)
+        controlView.playPauseButton.addTarget(self, action: #selector(playPauseAction(sender:)), for: .touchUpInside)
+        
+        playerView.translatesAutoresizingMaskIntoConstraints = false
+        playerView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        playerView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        playerView.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+        playerView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+    }
+    
+    func configure(url: URL) {
+        let playerItem = AVPlayerItem(url: url)
+        player.replaceCurrentItem(playerItem)
+    }
+    
+    @objc private func playPauseAction(sender: UIButton) {
+        switch player.status {
+        case .pause(_):
+            player.play()
+        default:
+            player.pause()
+        }
+    }
+
+}
+
+// TODO: 实现播放器的状态显示
+extension VideoView: VIPlayerDelegate {
+    
+    func player(_ player: VIPlayer, didLoadedFailed error: Error?) {
+        controlView.update(to: .pause)
+    }
+    
+    func player(_ player: VIPlayer, timeDidChange time: TimeInterval) {
+        controlView.currentTimeLabel.text = String(format: "%0.1f", time)
+    }
+    
+    func player(_ player: VIPlayer, statusDidChange status: VIPlayer.PlayerStatus) {
+        switch status {
+        case .pause(_):
+            controlView.update(to: .pause)
+        default:
+            controlView.update(to: .play)
+        }
+    }
+    
+}
+
+
+class VideoControlView: UIView {
+    
+    var currentTimeLabel: UILabel!
+    var playPauseButton: UIButton!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        currentTimeLabel = UILabel()
+        currentTimeLabel.text = "0"
+        currentTimeLabel.textColor = UIColor.contentColor
+        currentTimeLabel.font = UIFont.detailFont
+        addSubview(currentTimeLabel)
+        
+        playPauseButton = UIButton(type: .system)
+        addSubview(playPauseButton)
+        playPauseButton.setTitle("play", for: .normal)
+        
+        currentTimeLabel.translatesAutoresizingMaskIntoConstraints = false
+        playPauseButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        let leftConstraint = currentTimeLabel.leftAnchor.constraint(equalTo: leftAnchor)
+        leftConstraint.constant = 15
+        leftConstraint.isActive = true
+        currentTimeLabel.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        currentTimeLabel.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        
+        playPauseButton.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        playPauseButton.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        playPauseButton.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+    }
+    
+    enum ControlState {
+        case pause
+        case play
+    }
+    
+    var state: ControlState = .pause
+    
+    func update(to state: ControlState) {
+        switch state {
+        case .pause:
+            playPauseButton.setTitle("play", for: .normal)
+        case .play:
+            playPauseButton.setTitle("pause", for: .normal)
+        }
+    }
+    
+}
